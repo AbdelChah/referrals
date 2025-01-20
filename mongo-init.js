f7786380d67342d5baa5b60cac46abb9e74df315
Abdel <clexperiment@hotmail.com>
2025-01-20 12:17:49 +0200

    ngnix and mongo conf

--- mongo-init.js
+++ mongo-init.js
@@ -1,1 +1,18 @@
+
 rs.initiate();
+
+
+db = db.getSiblingDB("admin");
+db.createUser({
+    user: "root",
+    pwd: "securepassword",
+    roles: [{ role: "root", db: "admin" }]
+});
+
+
+db = db.getSiblingDB("referral_db");
+db.createUser({
+    user: "app_user",
+    pwd: "securepassword",
+    roles: [{ role: "readWrite", db: "referral_db" }]
+});
